b93587549e88ea799fa142d715bf174676998622
Cristian Eduardo <cristian.santos0308@gmail.com>
2024-10-12 22:56:07 -0300

    Seção Sobre Mim

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,11 +4,22 @@ const rootHtml = document.documentElement;
 function changeTheme() {
     const currentTheme = rootHtml.getAttribute("data-theme");
 
-    if(currentTheme === "dark") rootHtml.setAttribute("data-theme","light");
-    else rootHtml.setAttribute("data-theme","dark");
+    if (currentTheme === "dark") rootHtml.setAttribute("data-theme", "light");
+    else rootHtml.setAttribute("data-theme", "dark");
 
     toggleTheme.classList.toggle("bi-sun");
     toggleTheme.classList.toggle("bi-moon-stars");
 }
 
-toggleTheme.addEventListener('click',changeTheme);
+toggleTheme.addEventListener('click', changeTheme);
+
+
+const accordionHeaders = document.querySelectorAll(".accordion__header")
+
+accordionHeaders.forEach(header => {
+    header.addEventListener("click", () => {
+        const accordionItem = header.parentElement;
+        accordionItem.classList.toggle("active");
+    })
+}
+)
