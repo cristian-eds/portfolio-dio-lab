2219389080eeccda5e64bdd37a2c12935d4401fc
Cristian Eduardo <cristian.santos0308@gmail.com>
2024-10-12 23:38:05 -0300

    Adicionado controle link ativo e salvado tema no local storage

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,20 @@
 const toggleTheme = document.getElementById("toggleTheme");
 const rootHtml = document.documentElement;
+const themeActualLocalStorage = localStorage.getItem("theme");
+
+themeActualLocalStorage && rootHtml.setAttribute("data-theme", themeActualLocalStorage);
 
 function changeTheme() {
     const currentTheme = rootHtml.getAttribute("data-theme");
 
-    if (currentTheme === "dark") rootHtml.setAttribute("data-theme", "light");
-    else rootHtml.setAttribute("data-theme", "dark");
+    if (currentTheme === "dark") {
+        rootHtml.setAttribute("data-theme", "light")
+        localStorage.setItem("theme","light")
+    }
+    else {
+        rootHtml.setAttribute("data-theme", "dark")
+        localStorage.setItem("theme","dark")
+    };
 
     toggleTheme.classList.toggle("bi-sun");
     toggleTheme.classList.toggle("bi-moon-stars");
@@ -23,3 +32,16 @@ accordionHeaders.forEach(header => {
     })
 }
 )
+
+const menuLinks = document.querySelectorAll(".menu__link");
+
+menuLinks.forEach((item) => {
+    item.addEventListener(("click"), () => {
+        menuLinks.forEach((item) => {
+            item.classList.remove("active");
+        })
+        item.classList.add("active");
+    })
+})
+
+
